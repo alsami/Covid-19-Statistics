2d5bfd2444a5b74a9ad050128661ddcb24df4160
alsami <al.khatib.sami1990@gmail.com>
2020-03-16 07:39:58 +0100

    fix: use quotationmarks in apiurl

--- create-environment-prod-file.sh
+++ create-environment-prod-file.sh
@@ -7,5 +7,7 @@ echo writing new environment file
 
 echo "export const environment = {
     production: true,
-    apiUrl: ${apiUrl}
+    apiUrl: '${apiUrl}'
 };" > src/environments/environment.prod.ts
+
+cat src/environments/environment.prod.ts
