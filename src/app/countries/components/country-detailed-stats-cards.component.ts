e43987fce666151d6589b10e34ee0923028f7fcc
alsami <al.khatib.sami1990@gmail.com>
2020-03-21 16:09:11 +0100

    perf: set changedetection to on push

--- src/app/countries/components/country-detailed-stats-cards.component.ts
+++ src/app/countries/components/country-detailed-stats-cards.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CountryStats } from '@covid19/countries/models';
 
 @Component({
   selector: 'covid19-country-detailed-stats-cards',
   templateUrl: './country-detailed-stats-cards.component.html',
-  styleUrls: ['./country-detailed-stats-cards.component.scss']
+  styleUrls: ['./country-detailed-stats-cards.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CountryDetailedStatsCardsComponent {
   @Input() countryStats: CountryStats;
